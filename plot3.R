2c450fe82b5e351d6ea38e92c2e71e7fd26eee8d
hilaryroberts <hilary.maurice@gmail.com>
2015-08-09 01:22:38 +0100

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+#read in the data
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+data$timestamp <- strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+data1 <- subset(data, data$timestamp >= "2007-02-01 00:00:00" & data$timestamp < "2007-02-03 00:00:00")
+data1$Global_active_power <- as.numeric(as.character(data1$Global_active_power))
+
+#plot3
+plot(data1$timestamp, data1$Sub_metering_1, type = "l", ylab = "Energy Sub Metering", xlab = NA)
+lines(data1$timestamp, data1$Sub_metering_2, col = "red")
+lines(data1$timestamp, data1$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
