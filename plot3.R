68f83e9b6732780140d89919c968d89402e508d0
hilaryroberts <hilary.maurice@gmail.com>
2015-08-21 22:07:54 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 data$timestamp <- strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
 data1 <- subset(data, data$timestamp >= "2007-02-01 00:00:00" & data$timestamp < "2007-02-03 00:00:00")
-data1$Global_active_power <- as.numeric(as.character(data1$Global_active_power))
+data1$Global_active_power <- as.numeric(data1$Global_active_power)
 
 #plot3
 plot(data1$timestamp, data1$Sub_metering_1, type = "l", ylab = "Energy Sub Metering", xlab = NA)
